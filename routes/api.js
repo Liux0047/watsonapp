f44d0a2c0c86beae81bdbb297da4fb06a62cace1
Allen <tracy1kobe@gmail.com>
2016-02-06 17:38:53 +0800

    interest over time completed

--- routes/api.js
+++ routes/api.js
@@ -6,8 +6,9 @@ var express = require('express');
 var router = express.Router();
 var https = require('https');
 
-var API_KEY = 'd2c38d1f5fe8ae7a528f8f01d1ca36c291e23282';
-//var API_KEY = 'd8f8c787d46a28787e2119fff692e863d020d7da';
+var API_KEY_1 = 'd2c38d1f5fe8ae7a528f8f01d1ca36c291e23282';
+var API_KEY_2 = 'd8f8c787d46a28787e2119fff692e863d020d7da';
+var API_KEY_3 = 'ebd3a423e07ddaae345c6421485d36ff1a0ced11';
 
 var gateway = 'https://gateway-a.watsonplatform.net/calls/data/GetNews?outputMode=json&';
 
@@ -40,27 +41,25 @@ router.get('/ioc', function (req, res, next) {
         title: req.query.searchText
     };
 
-    doAjax(generateIOTUrl('positive', options), function  (result) {
-        var positiveCounts = result;
-        console.log(positiveCounts);
+    var responseCounter = 0;
 
-        doAjax(generateIOTUrl('negative', options), function  (result) {
-            var negativeCounts = result;
-            console.log(negativeCounts);
+    var responseData = {};
 
-            doAjax(generateIOTUrl('neutral', options), function  (result) {
-                var neutralCounts = result;
-                console.log(neutralCounts);
-
-                res.json({
-                    positiveCounts : positiveCounts,
-                    neutralCounts : neutralCounts,
-                    negativeCounts : negativeCounts
-                });
-            });
-        });
+    doAjax(buildIOTUrl('positive', options, API_KEY_1), function (result) {
+        responseData.positiveCounts = result;
+        sendIOTResponse(++responseCounter, 3, res, responseData);
     });
-    
+
+    doAjax(buildIOTUrl('negative', options, API_KEY_2), function (result) {
+        responseData.negativeCounts = result;
+        sendIOTResponse(++responseCounter, 3, res, responseData);
+    });
+
+    doAjax(buildIOTUrl('neutral', options, API_KEY_3), function (result) {
+        responseData.neutralCounts = result;
+        sendIOTResponse(++responseCounter, 3, res, responseData);
+
+    });
 });
 
 module.exports = router;
@@ -85,10 +84,15 @@ function doAjax(url, callback) {
 }
 
 
-function generateIOTUrl(sentiment, options) {
-    var url = gateway + 'start=now-10d&end=now&timeSlice=1d&q.enriched.url.title=' + options.title + '&' +
+function buildIOTUrl(sentiment, options, apiKey) {
+    var url = gateway + 'start=now-30d&end=now&timeSlice=1d&q.enriched.url.entities.entity=|text=' + options.title + ',type=company|&' +
         '&q.enriched.url.enrichedTitle.docSentiment=|type=' + sentiment + '|&' +
-        'apikey=' + API_KEY;
-    console.log('IOT URL generated: ' + url);
+        'apikey=' + apiKey;
     return url;
+}
+
+function sendIOTResponse(counter, threshold, res, responseData) {
+    if (counter >= threshold) {
+        res.json(responseData);
+    }
 }
